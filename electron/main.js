e3992ef1f6011c289a1ac028b1d165b30f908f69
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-15 15:51:55 -0300

    adaptações para o header

--- electron/main.js
+++ electron/main.js
@@ -1,16 +1,19 @@
 const { app, BrowserWindow} = require('electron')
 const path = require('path')
 
+require('./header/header-actions-main.js')
+
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         minWidth: 375,
         minHeight: 430,
         height: 800,
-        titleBarStyle: "hidden",
+        frame: false,
         icon: path.join( __dirname, "../assets/atom_icon.png"),
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
         }
     })
     win.setMenuBarVisibility(null)
