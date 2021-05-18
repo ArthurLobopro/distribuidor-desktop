741d8293eb7152fc63bc0c300c07b6afd5e18243
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-05-17 21:42:16 -0300

    Animação para aparecer adicionada

--- electron/browser-functions/alert.js
+++ electron/browser-functions/alert.js
@@ -1,12 +1,13 @@
 const target = document.getElementById("container")
 
-function alert({title, text, center = true}) {
+function alert({title, text, center = true, animation = true}) {
     title = title ?? window.location.hostname + " diz"
     const div = document.createElement('div')
     div.classList.add("full")
     div.id = "alert"
     div.innerHTML = `
-    <div id="alert-interior" style="align-self:${center ? "center": "flex-start;margin-top: 5px"}">
+    <div id="alert-interior" class="${animation ? "down" : ""}"
+    style="align-self:${center ? "center": "flex-start;margin-top: 5px"}">
         <div class="title">${title}</div>
         <div class="text"> ${text}</div>
         <button id="remove-alert">
