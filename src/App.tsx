3b310da2d664035a1b280f5fa67a38dae12aee2d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-07-28 18:59:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import Header from "./components/header"
 import Menu from "./components/Menu"
+import Result from "./components/Result"
+import Alert from "./components/Alert"
+
+import { useState } from "react"
 
 import "./index.css"
 import iconImg from "../assets/white-atom.png"
-console.log(iconImg);
 
 export default function App() {
+
+    const [state ,setState] = useState({result: [] , alert: {} })
+
+
     return (
         <>
             <Header windowName="Distribuidor Eletrônico" icon={iconImg}></Header>
             <div id="container">
-                <Menu/>
+                <Menu state={state} set={setState}/>
+                
+                <Result setState={setState} state={state}/>
+
+                <Alert setState={setState} state={state}/>
+                            
+                <footer>&copy;Arthur Lobo</footer>
             </div>
         </>
     )
