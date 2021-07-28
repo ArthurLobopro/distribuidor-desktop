0ac8bdae78e1e509882d13efaafefda92457ab7c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-07-28 20:25:23 -0300

    icon path resolved

--- electron/main.ts
+++ electron/main.ts
@@ -6,21 +6,27 @@ import './header/header-actions-main'
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
 
+const assetsPath =
+  process.env.NODE_ENV === 'production'
+    ? process.resourcesPath
+    : app.getAppPath()
+
 function createWindow () {
+    const iconPath = path.join(assetsPath, "/assets/atom_icon.png")
+
     const win = new BrowserWindow({
         width: 820,
         minWidth: 820,
         minHeight: 430,
         height: 800,
         frame: false,
-        icon: path.join( __dirname, "../assets/atom_icon.png"),
+        icon: iconPath,
         webPreferences: {
             nodeIntegration: true,
             preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
         }
     })
     win.setMenuBarVisibility(null)
-    win.setTitle("Distribuidor Eletrônico Desktop")
     win.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 }
 
